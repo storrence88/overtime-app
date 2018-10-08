8a2045fab3e0251fee496856f1a05dc40f0aa89e
Steven Torrence <storrence88@gmail.com>
2018-10-08 12:50:49 -0500

    Added edit functionality for post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@user = User.create(email: 'admin@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Steve', last_name: 'Torrence')
+@user = User.create(email: 'user1@test.com', password: 'password', password_confirmation: 'password', first_name: 'Steve', last_name: 'Torrence')
 
 puts '1 User has been created'
+
+AdminUser.create(email: 'admin@test.com', password: 'password', password_confirmation: 'password', first_name: 'Admin', last_name: 'User')
+
+puts '1 AdminUser has been created'
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rational content", user_id: @user.id)
